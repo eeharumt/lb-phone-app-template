4dbfdef14b15ae684e49f5b0c891154bd1cfb821
eeharumt <fukurou.origin@gmail.com>
2024-01-04 03:46:18 +0900

    add loading data from lua and adjust layout

--- svelte-ts/ui/src/routes/+layout.svelte
+++ svelte-ts/ui/src/routes/+layout.svelte
@@ -3,49 +3,50 @@
 	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
 
-	let isphoneWrapper = false;
 	let theme = 'dark';
 	const devMode = !(typeof window !== 'undefined' && window['invokeNative']);
+
+	function loaddata() {
+		window.getSettings().then((settings) => {
+			theme = settings.display.theme;
+		});
+	}
 
 	onMount(() => {
 		if (!browser) return;
 		if (devMode) {
 			document.body.style.visibility = 'visible';
-			isphoneWrapper = false;
 		} else {
-			isphoneWrapper = false;
-			window.addEventListener('message', (e) => {
-				if (e.data !== 'componentsLoaded') return;
-				getSettings().then((settings) => {
-					theme = settings.display.theme;
+			if (typeof window.getSettings === 'function') {
+				loaddata();
+			} else {
+				window.addEventListener('message', (event) => {
+					if (event.data === 'componentsLoaded') {
+						loaddata();
+					}
 				});
-				// onSettingsChange((settings) => {
-				// 	theme = settings.display.theme;
-				// });
-			});
+			}
 		}
 	});
 </script>
 
 <div id="app-root">
-	<h1>Svelte Sample</h1>
 	<slot />
 </div>
 
 <style>
 	#app-root {
-	  position: relative;
-	  width: 100%;
-	  height: 100vh;
-	  /*上下方向にはみ出した要素ををスクロールさせる*/
-	  overflow-y: scroll;
-	  /*スクロールバー非表示（IE・Edge）*/
-	  -ms-overflow-style: none;
-	  /*スクロールバー非表示（Firefox）*/
-	  scrollbar-width: none;
+		position: relative;
+		width: 100%;
+		height: 100vh;
+		/*上下方向にはみ出した要素ををスクロールさせる*/
+		overflow-y: scroll;
+		/*スクロールバー非表示（IE・Edge）*/
+		-ms-overflow-style: none;
+		/*スクロールバー非表示（Firefox）*/
+		scrollbar-width: none;
 	}
 	#app-root::-webkit-scrollbar {
-	  display: none;
+		display: none;
 	}
-  </style>
-  
+</style>
--- svelte-ts/ui/src/routes/+page.svelte
+++ svelte-ts/ui/src/routes/+page.svelte
@@ -2,9 +2,17 @@
 	let count = 0;
 </script>
 
-<div>
+<div class="sample-style">
+	<h1>Svelte Sample</h1>
 	<button class="btn btn-primary btn-wide text-white" on:click={() => count++}>CountUP</button>
-	<div>
+	<div >
 		Count: {count}
 	</div>
 </div>
+
+<style>
+	.sample-style {
+		padding: 80px 40px;
+		color: aqua;
+	}
+</style>
