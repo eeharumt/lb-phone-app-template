e98c7e4b318f487f6139decf316cb8ca92d0eee3
eeharumt <fukurou.origin@gmail.com>
2024-01-04 02:59:34 +0900

    init svelte commit

---
+++ svelte-ts/ui/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let count = 0;
+</script>
+
+<div>
+	<button class="btn btn-primary btn-wide text-white" on:click={() => count++}>CountUP</button>
+	<div>
+		Count: {count}
+	</div>
+</div>
